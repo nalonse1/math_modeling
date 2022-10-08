65be4fa51c8ff30cbf277592d4070fe67a978e78
nalonsel <99686708+nalonse1@users.noreply.github.com>
2022-10-08 12:03:09 +0000

    я сделал 1 лабу

---
+++ lec_1_data_since.py
@@ -0,0 +1,17 @@
+type
+
+type(3)
+
+print(type(3))
+
+print(type(3.3))
+
+print(type("молодец"))
+
+a = ["молодец", 1, "круто", 6, 8]
+
+print(type(a))
+
+print(type(True))
+
+print(type(None))
---
+++ lec_1_str.py
@@ -0,0 +1,9 @@
+a = "good"
+b = 'bad'
+#print(a-b)
+print(a*3)
+
+c = 50
+d = 10 
+print( f'{c} {d}')
+print(len(a))
